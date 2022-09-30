81d925f6abd78aa9c2bb9210e8ee935e9137fe57
Pyrogrammer404 <105351111+Pyrogrammer404@users.noreply.github.com>
2022-10-01 00:36:49 +0530

    Update LR.py
    Updated older Libraries to run the code efficiently.

--- Linear Regression/LR.py
+++ Linear Regression/LR.py
@@ -8,18 +8,20 @@ import datetime as dt
 from sklearn.metrics import mean_squared_error, r2_score
 files=["BTCUSD.csv","DASHUSD.csv","DOGEUSD.csv","ETHUSD.csv","LTCUSD.csv","XMRUSD.csv"];
 for i in files:
-    df = pd.read_csv(i)
-    print(df.head())
-    df = df[['Date','Close']] 
-    df['Date'] = pd.to_datetime(df.Date,format='%d/%m/%Y')
-    df['Date']=df['Date'].map(dt.datetime.toordinal)
-    print(df.head())
-    X = df.iloc[:, 0].values.reshape(-1, 1)  
-    Y = df.iloc[:, 1].values.reshape(-1, 1) 
+    data = pd.read_csv(i)
+    #Added comment 1
+    data = data[['Date','Close']] 
+    data['Date'] = pd.to_datetime(data.Date,format='%d/%m/%Y')
+    data['Date']=data['Date'].map(dt.datetime.toordinal)
+    #Added comment 2
+    X = data.iloc[:, 0].values.reshape(-1, 1)  
+    Y = data.iloc[:, 1].values.reshape(-1, 1) 
     x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+    #Added comment 3
     linear_regressor = LinearRegression()
     linear_regressor.fit(x_train, y_train)
     y_pred = linear_regressor.predict(x_test)
+    #Added comment 4
     plt.scatter(x_test, y_test,  color='green')
     plt.plot(x_test, y_pred, color='red', linewidth=4)
     plt.title(i)
